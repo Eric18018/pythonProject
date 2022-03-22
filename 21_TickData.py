cb6482aae967650d27557119f74e89f06ca1f75c
Eric <398655714@qq.com>
2022-03-23 00:05:58 +0800

    add new 21

--- 21_TickData.py
+++ 21_TickData.py
@@ -3,7 +3,7 @@ import tushare as ts
 
 if __name__ == '__main__':
     # 分时数据接口暂不能用
-    df_tick = ts.get_tick_data('002372', date='2022-01-23', src='tt')
+    df_tick = ts.get_tick_data('002372', date='2020-01-23', src='tt')
     df_tick.index = pd.to_datetime(df_tick.time)
     df_tick.drop(axis=1, columns='time', inplace=True)
     print(df_tick[2000:2010])
